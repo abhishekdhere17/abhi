89189000936498d9b3a00099457c6d94f6878016
abhishekdhere17 <141109623+abhishekdhere17@users.noreply.github.com>
2025-02-11 09:18:29 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,4 +1,7 @@
-num1=int(input("enter num 1="))
-num2=int(input("enter num 2+"))
-print(num1+num2)
+num1=int(input("enter num 1= "))
+num2=int(input("enter num 2= "))
+print("addition",num1+num2)
+print("subtraction",num1-num2)
+print("multiply",num1*num2)
+print("Divide",num1/num2)
 
